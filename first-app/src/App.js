47058d45dafc94e10a4651f18dba0ed973408fc9
Ronald Guerrero <ronald.a.guerrero@gmail.com>
2019-05-08 12:11:41 -0700

    updating files

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+// createElement accepts(type, props, children)
+
+// const myList = (
+//   <ul>
+//     <li>apple</li>
+//     <li>cookie</li>
+//     <li>sushi</li>
+//   </ul>  
+// );
+
+// ReactDOM.render(myList, document.getElementById('app2'));
+
 const myButton = React.createElement('button', null, 'Click Me');
   console.log(JSON.stringify(myButton));
   /*
@@ -19,6 +31,22 @@ const myButton = React.createElement('button', null, 'Click Me');
     }
   */
   ReactDOM.render(myButton, document.getElementById('app'));
+
+
+var d = new Date();
+var n = d.toLocaleTimeString();
+
+// document.getElementById('time').innerHTML = n;
+
+
+const timeButton = React.createElement(n, null, 'Time');
+
+// var intervalID = window.setInterval(myCallback, 500);
+
+function myCallback() {
+  // ReactDOM.render(timeButton, document.getElementById('time').innerHTML = n);
+}
+  
 
 function App() {
   return (
